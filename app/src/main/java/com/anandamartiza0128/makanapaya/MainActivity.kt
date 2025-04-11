d920ad5183342146ee40939fdf53ebd39d57d9dc
Ananda Martiza Febrina <anandamartiza@student.telkomuniversity.ac.id>
2025-04-11 22:03:33 +0700

    Menambahkan gambar

--- app/src/main/java/com/anandamartiza0128/makanapaya/MainActivity.kt
+++ app/src/main/java/com/anandamartiza0128/makanapaya/MainActivity.kt
@@ -5,11 +5,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
@@ -24,6 +26,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -85,8 +88,16 @@ fun ScreenContent(modifier: Modifier = Modifier, navController: NavHostControlle
     ) {
         Column(
             horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.spacedBy(12.dp) // jarak antar tombol
+            verticalArrangement = Arrangement.spacedBy(12.dp)
         ) {
+            // Tambahkan gambar di atas tombol
+            Image(
+                painter = painterResource(id = R.drawable.ic_launcher),
+                contentDescription = stringResource(R.string.app_name),
+                modifier = Modifier
+                    .size(120.dp) // ubah ukuran sesuai kebutuhan
+            )
+
             // Tombol Tambah Makanan
             Button(
                 onClick = {
