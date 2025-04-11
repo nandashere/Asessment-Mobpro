e16cc53db9ef36c2b96c48ec6f59c7f6fd591bac
Ananda Martiza Febrina <anandamartiza@student.telkomuniversity.ac.id>
2025-04-11 23:56:25 +0700

    menambahkan scrollable

--- app/src/main/java/com/anandamartiza0128/makanapaya/navigation/TambahMakanan.kt
+++ app/src/main/java/com/anandamartiza0128/makanapaya/navigation/TambahMakanan.kt
@@ -13,7 +13,9 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.Image
@@ -117,6 +119,7 @@ fun TambahMakananScreen(
             modifier = Modifier
                 .padding(innerPadding)
                 .padding(16.dp)
+                .verticalScroll(rememberScrollState())
                 .fillMaxSize(),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
